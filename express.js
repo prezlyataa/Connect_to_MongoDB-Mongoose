f5d8068b18b2bed432e6a8aab0d2fc99c6e312bb
Yuriy <prezlyata.yuriy@gmail.com>
2018-01-11 12:27:27 +0200

    first commit

---
+++ express.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+var app = express();
+
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+    console.log('connection error', err);
+});
+
+db.once('open', function () {
+    console.log('Connected to Database');
+});
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    name : String,
+    age : Number
+});
+
+var User = mongoose.model('User', userSchema);
+
+var student = new User({
+    name : 'Arnold',
+    age : 21
+});
+
+student.save(function (err, data) {
+    if (err) console.log(err);
+    else console.log('Saved : ', data );
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
